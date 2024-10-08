9a66b0da95472063e34e55edeac388ac318c7e36
tamjidakash100 <tamjidakash100@gmail.com>
2024-10-08 12:37:28 +0600

    Test simple PWA

---
+++ DotNetPWA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetPWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
